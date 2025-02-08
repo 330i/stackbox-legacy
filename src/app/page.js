c185469f08a373037f8771ef8ab477e1cd47d6f8
330i <id.kim.dev@proton.me>
2025-02-08 00:47:02 -0600

    Added Threebox

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,82 @@
-import Image from "next/image";
+'use client'
+
+import { useRef, useEffect } from "react";
+import mapboxgl from 'mapbox-gl';
+import Threebox from 'threebox-plugin/src/Threebox';
+import * as THREE from 'three';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const mapRef = useRef();
+    const mapContainerRef = useRef();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    useEffect(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaWtzb21ldGhpbmdzb21ldGhpbmczODIzIiwiYSI6ImNseDk4d3c1MDJyYWkybXB2cXgzeGZqZ2kifQ.nFz0xuCOfn17Kj0bu2ahiw';
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/light-v11',
+            center: [-95.3656135755987, 29.76014807317625],
+            zoom: 17,
+            pitch: 60,
+        });
+        
+        mapRef.current.on('style.load', () => {
+            mapRef.current.addLayer({
+                id: 'stacking-plan',
+                type: 'custom',
+                renderingMode: '3d',
+                paint: {
+                    'fill-color': '#ff0000'
+                },
+                onAdd: () => {
+                    window.tb = new Threebox(
+                        mapRef.current,
+                        mapRef.current.getCanvas().getContext('webgl'),
+                        { defaultLights: true }
+                    );
+                    const scale = 20;
+                    const options = {
+                        obj: '/tower.glb',
+                        type: 'glb',
+                        scale: { x: scale, y: scale, z: scale },
+                        units: 'meters',
+                        rotation: { x: 90, y: 45, z: 0 },
+                    };
+
+                    var dl = new THREE.DirectionalLight(0xff0000);
+                    dl.position.set(0, -70, 100).normalize();
+                    window.tb.scene.add(dl);
+                    var dl2 = new THREE.DirectionalLight(0xff0000);
+                    dl2.position.set(0, 70, 100).normalize();
+                    window.tb.scene.add(dl2);
+
+                    var geometry1 = new THREE.BoxGeometry(60, 60, 60);
+                    var material1 = new THREE.MeshStandardMaterial({
+                        color: new THREE.Color(0xff0000),
+                        side: THREE.DoubleSide,
+                        clippingPlanes: [
+                            new THREE.Plane(new THREE.Vector3(0, 0, 0), 5.1)
+                        ],
+                        clipIntersection: true
+                    });
+
+                    window.tb.loadObj(options, (model) => {
+                        model.setCoords([-95.36576714742297, 29.76046335699732]);
+                        model.setRotation({ x: 0, y: 0, z: 235 });
+                        window.tb.add(model);
+                    });
+                },
+                render: () => {
+                    window.tb.update();
+                }
+            });
+        });
+
+        return () => mapRef.current.remove();
+    }, []);
+
+    return (
+        <div className="overflow-hidden">
+            <div id="map-container" ref={mapContainerRef} className="w-screen h-screen"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
